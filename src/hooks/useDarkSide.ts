1e0e3ba754be4ec5455a85365a1685de1c47b23b
MessiasFMS <felipe.messias.silva@outlook.com>
2023-11-30 02:09:16 -0300

    Ajustes

--- src/hooks/useDarkSide.ts
+++ src/hooks/useDarkSide.ts
@@ -1,17 +1,26 @@
-import { useState, useEffect } from "react";
- 
-export default function useDarkSide() {
-    const [theme, setTheme] = useState(localStorage.theme);
-    const colorTheme = theme === "dark" ? "light" : "dark";
-    localStorage.setItem("theme", theme);
-    useEffect(() => {
-        const root = window.document.documentElement;
-        root.classList.remove(colorTheme);
-        root.classList.add(theme);
-        if (localStorage.theme == "dark")
-            localStorage.removeItem("theme");
-        else localStorage.setItem("theme", theme);
-    }, [theme, colorTheme]);
- 
-    return [colorTheme, setTheme];
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+type Theme = "dark" | "light";
+
+export default function useDarkSide(): [Theme, Dispatch<SetStateAction<Theme>>] {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = localStorage.getItem("theme") as Theme;
+    return storedTheme || "dark"; // Set a default theme if none is stored
+  });
+
+  const colorTheme: Theme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    if (localStorage.theme === "dark") {
+      localStorage.removeItem("theme");
+    } else {
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme, colorTheme]);
+
+  return [colorTheme, setTheme];
 }
